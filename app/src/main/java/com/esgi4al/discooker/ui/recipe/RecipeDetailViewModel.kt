839b887574d50faa2a649b87f10770ca2ddc174c
goodfoodtruck <mielatomique@protonmail.com>
2024-10-11 00:29:31 +0200

    initialize RecipeDetailFragment

---
+++ app/src/main/java/com/esgi4al/discooker/ui/recipe/RecipeDetailViewModel.kt
@@ -0,0 +1,8 @@
+package com.esgi4al.discooker.ui.recipe
+
+import androidx.lifecycle.ViewModel
+import com.esgi4al.discooker.models.RecipeModel
+
+class RecipeDetailViewModel : ViewModel() {
+    lateinit var recipe: RecipeModel
+}
