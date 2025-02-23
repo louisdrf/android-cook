f10c983ce4c6da8b0f906d896dbbc613dfef1cfb
Louis Driffort <louis.driffort@gmail.com>
2025-02-23 23:10:00 +0100

    remove unused methods

--- app/src/main/java/com/esgi4al/discooker/repositories/HomePageGlobalDataRepository.kt
+++ app/src/main/java/com/esgi4al/discooker/repositories/HomePageGlobalDataRepository.kt
@@ -28,35 +28,6 @@ class HomePageGlobalDataRepository {
         }
     }
 
-    suspend fun getAllRecipesByCategoryName(categoryName: String): List<Recipe>? {
-        return try {
-            withContext(Dispatchers.IO) {
-                globalDataService.getRecipesByCategory(categoryName)
-            }
-        } catch (e: HttpException) {
-            Log.e("API_ERROR", "HTTP Error: ${e.code()} - ${e.message()}")
-            null
-        } catch (e: Exception) {
-            Log.e("API_ERROR", "Network Error: ${e.message}")
-            null
-        }
-    }
-
-
-    suspend fun getAllRecipesByRegionName(regionName: String): List<Recipe>? {
-        return try {
-            withContext(Dispatchers.IO) {
-                globalDataService.getRecipesByRegion(regionName)
-            }
-        } catch (e: HttpException) {
-            Log.e("API_ERROR", "HTTP Error: ${e.code()} - ${e.message()}")
-            null
-        } catch (e: Exception) {
-            Log.e("API_ERROR", "Network Error: ${e.message}")
-            null
-        }
-    }
-
     suspend fun getAllCategories(): List<Category>? {
         return try {
             withContext(Dispatchers.IO) {
