73966b72b8e578f735d511561023f4fd7ea3355e
Louis Driffort <louis.driffort@gmail.com>
2025-02-24 21:01:37 +0100

    create users recycler view adapter

---
+++ app/src/main/java/com/esgi4al/discooker/ui/recyclerViewAdapters/UsersRvAdapter.kt
@@ -0,0 +1,36 @@
+package com.esgi4al.discooker.ui.recyclerViewAdapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.esgi4al.discooker.R
+import com.esgi4al.discooker.models.ListableUser
+import com.esgi4al.discooker.ui.viewHolders.UserViewHolder
+
+class UsersRvAdapter(
+    private val users: List<ListableUser>,
+) : RecyclerView.Adapter<UserViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.listable_user, parent, false)
+        return UserViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val user = this.users[position]
+
+        Glide
+            .with(holder.itemView)
+            .load("https://robohash.org/${user.username}")
+            .into(holder.userProfileImage)
+
+        holder.userName.text = user.username
+
+        holder.userNbCumulatedLikes.text = "10"
+        holder.userNbCumulatedRecipes.text = "10"
+    }
+
+    override fun getItemCount(): Int = users.size
+}
