c84ab51286c1bcb47596e3a34adea6f94f9f0b6a
Louis Driffort <louis.driffort@gmail.com>
2025-02-24 21:02:00 +0100

    setup search bar and users recycler view

--- app/src/main/java/com/esgi4al/discooker/ui/fragments/UsersListFragment.kt
+++ app/src/main/java/com/esgi4al/discooker/ui/fragments/UsersListFragment.kt
@@ -1,17 +1,33 @@
 package com.esgi4al.discooker.ui.fragments
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.esgi4al.discooker.R
+import com.esgi4al.discooker.models.ListableUser
 import com.esgi4al.discooker.repositories.UsersListDataRepository
+import com.esgi4al.discooker.ui.recyclerViewAdapters.UsersRvAdapter
 import com.esgi4al.discooker.ui.viewModels.UsersListViewModel
 import com.esgi4al.discooker.ui.viewModels.factories.UsersListViewModelFactory
 
 class UsersListFragment: Fragment() {
+
+    private lateinit var usersListRv: RecyclerView
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+    private lateinit var searchResultsTv: TextView
+    private lateinit var searchView: androidx.appcompat.widget.SearchView
+    private var currentSearch: String? = ""
+    private var searchRunnable: Runnable? = null
+    private val searchHandler = Handler(Looper.getMainLooper())
 
     private val usersListViewModel: UsersListViewModel by viewModels {
         UsersListViewModelFactory(UsersListDataRepository(), this)
@@ -22,10 +38,55 @@ class UsersListFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val view = inflater.inflate(R.layout.fragment_users_list, container, false)
+        swipeRefreshLayout = view.findViewById(R.id.users_list_swipe_refresh_layout)
+        searchView = view.findViewById(R.id.searchBarUsers)
+        searchResultsTv = view.findViewById(R.id.users_search_results_tv)
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setUpSearchView()
+
+        usersListViewModel.users.observe(viewLifecycleOwner) { users ->
+            val searchResultsText = if (users.isEmpty()) {
+                "Aucun utilisateur ne correspond à votre recherche."
+            } else {
+                "${users.size} utilisateur(s) trouvé(s)."
+            }
+            searchResultsTv.text = searchResultsText
+            setUpUsersRv(users, view)
+            swipeRefreshLayout.isRefreshing = false
+        }
+    }
+
+    private fun setUpSearchView() {
+        searchView.setOnQueryTextListener(object : androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                swipeRefreshLayout.isRefreshing = false
+                currentSearch = newText
+
+                searchRunnable?.let { searchHandler.removeCallbacks(it) }
+
+                searchRunnable = Runnable {
+                    usersListViewModel.fetchAllUsers()
+                }
+
+                searchHandler.postDelayed(searchRunnable!!, 200)
+                return true
+            }
+        })
+    }
+
+
+    private fun setUpUsersRv(users: List<ListableUser>, fragmentView: View) {
+        usersListRv = fragmentView.findViewById(R.id.users_list_rv)
+        usersListRv.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        usersListRv.adapter = UsersRvAdapter(users)
     }
 }
