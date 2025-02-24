f795fb4a71381c84e5151d9a3e10e790a051e1ca
Louis Driffort <louis.driffort@gmail.com>
2025-02-24 19:52:14 +0100

    create and call at init {} fetch all users method

--- app/src/main/java/com/esgi4al/discooker/ui/viewModels/UsersListViewModel.kt
+++ app/src/main/java/com/esgi4al/discooker/ui/viewModels/UsersListViewModel.kt
@@ -1,11 +1,32 @@
 package com.esgi4al.discooker.ui.viewModels
 
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.esgi4al.discooker.models.ListableUser
 import com.esgi4al.discooker.repositories.UsersListDataRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class UsersListViewModel(
-    private val globalDataRepo: UsersListDataRepository,
+    private val globalUsersDataRepo: UsersListDataRepository,
     val context: LifecycleOwner
 ): ViewModel() {
+
+    private val _users = MutableLiveData<List<ListableUser>>()
+    val users: LiveData<List<ListableUser>> get() = _users
+
+    init {
+        fetchAllUsers()
+    }
+
+    private fun fetchAllUsers() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val fetchedUsers = globalUsersDataRepo.getAllUsers() ?: emptyList()
+            _users.postValue(fetchedUsers)
+        }
+    }
+
 }
